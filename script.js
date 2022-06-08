d20335f13fcced3069ceb1252a99fc32fc309725
Lucas Arruda <arrudafdc@gmail.com>
2022-06-08 16:38:41 -0300

    Finished JS

---
+++ script.js
@@ -0,0 +1,30 @@
+const addInput = document.querySelector('.add-input');
+const addButton = document.querySelector('.add-btn');
+const taskList = document.querySelector('.task-list');
+
+let content;
+addInput.addEventListener('change', (event) => {
+  content = event.target.value;
+});
+
+addInput.addEventListener('keydown', handleEvent);
+
+function handleEvent(event) {
+  if (event.key == 'Enter') {
+    createElement(this.value);
+  }
+}
+
+addButton.addEventListener('click', () => {
+  createElement(content);
+});
+
+function createElement(text) {
+  const element = document.createElement('div');
+  const key = Math.random();
+  element.classList.add('list-item');
+  element.innerHTML = `<input id="${key}" type="checkbox"><label for="${key}">${text}</label><button>X</button>`;
+  taskList.appendChild(element);
+  const btn = element.querySelector('button');
+  btn.addEventListener('click', () => element.remove());
+}
